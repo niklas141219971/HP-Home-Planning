b5d489fe38be6caece76b0ff156e5409a202a374
nmahler1997@gmx.de <KP1NMA@stadtwerke-flensburg.de>
2020-09-17 14:25:55 +0200

    separate header and footer from index for usage in all subsequent pages

---
+++ src/footer.php
@@ -0,0 +1,11 @@
+    <footer class="bd-footer text-muted">
+        <div class="container-fluid p-3 p-md-5">
+            <ul class="bd-footer-links">
+                <li><a href="https://github.com/Nabeelmeassar">Github Nabeel Meassar</a></li>
+                <li><a href="https://github.com/niklas141219971">GitHub Niklas Mahler</a></li>
+            </ul>
+            <p> Diese Seite wurde als Schulprojekt erstellt </p>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ src/header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="de">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HP - Home Planning</title>
+  <link rel ="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-dark bg-dark navbar-expand-md fixed-top">
+            <a class="navbar-brand" href="index.php">
+                <img src="logo.png" class="rounded" alt="logo" width ="80">
+            </a>
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="index.php">Home</a>
+                <a class="nav-item nav-link" href="#">Placeholder</a>
+                <a class="nav-item nav-link" href="#">Placeholder</a>
+                <a class="nav-item nav-link navitem-right" href="#">Signup</a>        
+            </div>
+        </nav>
+    </header>
